b91d370e0c5dd56c69bafa4615acf8ea11ca7ba7
Alan Marin De Luna <alan.marin1996@email.bakersfieldcollege.edu>
2018-07-26 20:37:29 -0700

    Initial Commit

---
+++ WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,99 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by Alan Marin De Luna on 7/8/18.
+//  Copyright © 2018 Alan Marin De Luna. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+//class name has to be the same as the file
+class ConversionViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    
+    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
+        if let text = textField.text, let value = Double(text) {
+            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+        } else {
+            fahrenheitValue = nil
+        }
+    }
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer){
+        textField.resignFirstResponder()
+    }
+    
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        } else {
+            return nil
+        }
+    }
+    
+    func updateCelsiusLabel() {
+        if let celsiusValue = celsiusValue {
+            celsiusLabel.text =
+                numberFormatter.string(from: NSNumber(value: celsiusValue.value))
+        } else{
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Logging testing
+        print("ConversionViewController loaded its view")
+        
+        updateCelsiusLabel()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        //  psuedocode for changing background color depending on time of day
+        //  if(6:00PM < t < 7:00AM) -> self.view.ba ckgroundColor = UIColor.black
+        //  else {
+        //          self.view.backgroundColor = UIColor.white
+        //       }
+    }
+    
+    func textField(_ textField: UITextField,
+                   shouldChangeCharactersIn range: NSRange,
+                   replacementString string: String) -> Bool {
+        
+        let existingTextHasDecimalSeperator = textField.text?.range(of: ".")
+        let replacementTextHasDecimalSeperator = string.range(of: ".")
+        
+        if existingTextHasDecimalSeperator != nil,
+            replacementTextHasDecimalSeperator != nil {
+            return false
+        } else {
+            return true
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
